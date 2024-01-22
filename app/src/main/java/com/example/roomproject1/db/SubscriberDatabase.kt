aaa7ef9b93db5d3385feb5020d5860904075379a
Anant Desai <anantdesai22@gmail.com>
2024-01-22 17:22:25 +0530

    Added SubscriberViewModel and with this added basic functionality of adding subscriber into database and clearAll

--- app/src/main/java/com/example/roomproject1/db/SubscriberDatabase.kt
+++ app/src/main/java/com/example/roomproject1/db/SubscriberDatabase.kt
@@ -1,9 +1,11 @@
 package com.example.roomproject1.db
 
 import android.content.Context
+import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
+@Database(entities = [Subscriber::class], version = 1)
 abstract class SubscriberDatabase : RoomDatabase() {
 
     abstract val subscriberDAO : SubscriberDAO
